09889f784685ac85ae714a438eafae0595246a19
np4abdou1 <197599897+np4abdou1@users.noreply.github.com>
2025-12-05 03:48:34 +0100

    hi

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name='filter',version='1.0.0',packages=['filter'],python_requires='>=3.6')
+setup(name='joyboyw',version='1.0.0',packages=['filter'],python_requires='>=3.6')
